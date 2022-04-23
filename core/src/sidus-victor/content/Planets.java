50a0b4daa8c93daa6f1e1b29365c91db6569f96b
youngterror3 <75321695+youngterror3@users.noreply.github.com>
2022-04-23 07:37:07 -0700

    Add files via upload

---
+++ core/src/sidus-victor/content/Planets.java
@@ -0,0 +1,49 @@
+package sidus-victor.content;
+
+import arc.graphics.*;
+import sidus-victor.ctype.*;
+import sidus-victor.graphics.*;
+import sidus-victor.graphics.g3d.*;
+import sidus-victor.maps.planet.*;
+import sidus-victor.type.*;
+
+public class Planets implements ContentList{
+    public static Planet
+    sun,
+    serpulo;
+
+    @Override
+    public void load(){
+        sun = new Planet("sun", null, 4f){{
+            bloom = true;
+            accessible = false;
+
+            meshLoader = () -> new SunMesh(
+                this, 4,
+                5, 0.3, 1.7, 1.2, 1,
+                1.1f,
+                Color.valueOf("ff7a38"),
+                Color.valueOf("ff9638"),
+                Color.valueOf("ffc64c"),
+                Color.valueOf("ffc64c"),
+                Color.valueOf("ffe371"),
+                Color.valueOf("f4ee8e")
+            );
+        }};
+
+        serpulo = new Planet("serpulo", sun, 1f, 3){{
+            generator = new SerpuloPlanetGenerator();
+            meshLoader = () -> new HexMesh(this, 6);
+            cloudMeshLoader = () -> new MultiMesh(
+                new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Pal.spore).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
+                new HexSkyMesh(this, 1, 0.6f, 0.16f, 5, Color.white.cpy().lerp(Pal.spore, 0.55f).a(0.75f), 2, 0.45f, 1f, 0.41f)
+            );
+            atmosphereColor = Color.valueOf("3c1b8f");
+            atmosphereRadIn = 0.02f;
+            atmosphereRadOut = 0.3f;
+            startSector = 15;
+            alwaysUnlocked = true;
+            landCloudColor = Pal.spore.cpy().a(0.5f);
+        }};
+    }
+}
